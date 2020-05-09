dd2ecd4e70f757df85759c67f69e70a37192302f
Stefano Raneri <45082162+St3veR0nin@users.noreply.github.com>
2020-05-09 14:10:16 +0200

    Add files via upload

---
+++ cbaio.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import time
+
+class CleverBot:
+
+
+    def __init__(self):
+        """
+        Requirements: pyppeteer, asyncio and bs4
+        """
+        self.element = None
+        options = Options()
+        options.headless = True 
+        self.wd = webdriver.Firefox(options=options)
+
+    def init(self):
+        """
+        Start the webdriver in headless mode, and install chromium 
+        if not available, must be called just one time, until close()
+        is called.
+        """
+        self.wd.get("https://www.cleverbot.com/") 
+        self.element =  self.wd.find_element_by_css_selector("#avatarform > input.stimulus")
+
+
+    def getResponse(self, text):
+        """
+        Send text to Cleverbot and recieve response
+        """
+        self.element.send_keys(text)
+        self.element.submit()
+        time.sleep(4)
+        return self.wd.find_element_by_css_selector("#line1 > span.bot").text
+
+    def close(self):
+        """
+        Close the connection to Cleverbot and closes page and browser
+        """
+        self.wd.quit()
